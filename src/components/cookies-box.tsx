306ee3c65b57838f04a0118d6bad5e86f13c89d3
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-18 19:57:04 -0300

    feat: :sparkles: add cookie saver

--- src/components/cookies-box.tsx
+++ src/components/cookies-box.tsx
@@ -1,9 +1,44 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./button";
 
+import { COOKIE_KEY, CookieDecision } from "@/config/cookies";
+import { getCookie, setCookie } from "cookies-next";
+import { useEffect, useState } from "react";
+
 export function CookiesBox() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const cookie = getCookie(COOKIE_KEY) as CookieDecision;
+    if (
+      !cookie ||
+      cookie.length === 0 ||
+      !Object.values(CookieDecision).includes(cookie)
+    ) {
+      setShow(true);
+      return;
+    }
+
+    setShow(false);
+  }, []);
+
+  function handleAccept() {
+    setCookie(COOKIE_KEY, CookieDecision.ALLOW);
+    setShow(false);
+  }
+
+  function handleReject() {
+    setCookie(COOKIE_KEY, CookieDecision.DENY);
+    setShow(false);
+  }
+
   return (
-    <div className="w-full fixed bottom-8 flex justify-center flex-row z-40 md:px-3 pointer-events-none">
+    <div
+      data-show={show}
+      className="w-full fixed hidden data-[show=true]:flex bottom-8 justify-center flex-row z-40 md:px-3 pointer-events-none"
+    >
       <div className="mx-4 py-4 px-4 md:py-2 md:pl-4 md:pr-2 bg-white rounded-lg shadow-2xl md:shadow-lg flex flex-col md:flex-row gap-4 items-center pointer-events-auto">
         <span>
           Nós utilizamos cookies para melhorar sua experiência. Você pode optar
@@ -18,8 +53,10 @@ export function CookiesBox() {
           .
         </span>
         <div className="w-full flex flex-col md:items-stretch md:w-auto md:flex-row gap-2">
-          <Button className="w-full">Aceitar</Button>
-          <Button variant="outline" className="w-full">
+          <Button className="w-full" onClick={handleAccept}>
+            Aceitar
+          </Button>
+          <Button variant="outline" className="w-full" onClick={handleReject}>
             Rejeitar
           </Button>
         </div>
---
+++ src/config/cookies.ts
@@ -0,0 +1,6 @@
+export const COOKIE_KEY = "marketing-cookie-decision";
+
+export enum CookieDecision {
+  ALLOW = "allow",
+  DENY = "deny",
+}
